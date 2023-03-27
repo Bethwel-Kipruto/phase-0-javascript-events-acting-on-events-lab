6e50ad6d58711855106f58c07014778a844138d6
BETHWEL <bethwelk85@gmail.com>
2023-03-27 14:12:56 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Your code here
+const dodger = document.getElementById("dodger");
+
+
+function moveDodgerLeft() {
+    const left = parseInt(dodger.style.left);
+   
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+
+
+
+
+  function moveDodgerRight() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers);
+  
+    if (left > 0) {
+      dodger.style.left = `${left + 1}px`;
+    }
+  }
